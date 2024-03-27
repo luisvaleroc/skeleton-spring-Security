1f5c37d6a1c840fabf01072f3b9787a4a0a5088d
Luis <luisvaleroc@gmail.com>
2024-03-27 18:56:48 -0300

    basic auth

---
+++ src/main/java/com/app/app/SpringHubApplication.java
@@ -0,0 +1,13 @@
+package com.app.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/app/controllers/TestAuthController.java
@@ -0,0 +1,31 @@
+package com.app.app.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+
+@PreAuthorize("denyAll()")
+public class TestAuthController {
+
+    @GetMapping("/hello")
+    @PreAuthorize("permitAll()")
+    public  String hello(){
+        return "hello world2";
+    }
+
+    @GetMapping("/hello-secured")
+    @PreAuthorize("hasAnyAuthority('READ')")
+    public  String helloSecured(){
+        return "hello world secured";
+    }
+
+
+    @GetMapping("/hello-secured2")
+    public  String helloSecured2(){
+        return "hello world secured2";
+    }
+}
